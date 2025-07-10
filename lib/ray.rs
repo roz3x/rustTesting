d71e1067ae8bf8aa7632a73c9297cc69d12e507d
Shivang Upadhyay <shivangu@linux.ibm.com>
2025-07-10 22:26:45 +0530

    gradient chapter

--- lib/ray.rs
+++ lib/ray.rs
@@ -1,4 +1,6 @@
 use crate::vec3::Vec3;
+use crate::vec3::Color;
+
 
 #[derive(Clone, Copy)]
 pub struct Ray {
@@ -7,7 +9,16 @@ pub struct Ray {
 }
 
 impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Self { origin, direction }
+    }
+
     pub fn at(self, t: f32) -> Vec3 {
         self.direction.clone().mul(t).add(self.origin)
+    }
+
+    pub fn get_color(self) -> Color {
+        let a = (self.direction.clone().unit_vec().y + 1.0) * 0.5 ;
+        Color::new(0.0, 0.0, 0.0).mul(1.0 - a).add(Color::new(0.5,0.7,1.0).mul(a))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Binary;
 use std::fs::{File, OpenOptions};
 use std::io::Write;
 
@@ -8,6 +9,7 @@ mod vec3;
 
 use vec3::Color;
 use vec3::Vec3;
+use ray::Ray;
 struct Ppm {
     height: usize,
     width: usize,
@@ -88,29 +90,52 @@ impl Ppm {
 }
 
 pub fn main() {
-    let mut ppm = Ppm::new("output.ppm", 256, 256);
+
+    let aspect_ratio = 9.0 / 16.0;
+    let image_width = 400;
+    let image_height = (image_width as f32 / aspect_ratio) as usize;
+    let image_height = if image_height > 1 { image_height } else { 1 };
+
+    let mut ppm = Ppm::new("output.ppm", image_height, image_width);
     ppm.create_file().add_header();
 
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
-    let image_height = image_width / aspect_ratio as usize;
-    let image_height = if image_height > 1 { image_height } else { 1 };
 
     let focal_length = 1.0 as f32;
     let viewport_height = 2.0 as f32;
     let viewport_width = viewport_height * (image_width as f32 / image_height as f32);
 
     let camera_center = Vec3::new(0.0, 0.0, 0.0);
-    let viewport_u  = Vec3::new(viewport_width , 0.0, 0.0 );
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
     let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
-    for i in 0..256 {
-        for j in 0..256 {
-            print!("\r[scanline remaining: {}]", (256 - i));
-            let r = (i as f32) / 255.0;
-            let g: f32 = (j as f32) / 255.0;
+    let pixel_delta_u = viewport_u.clone().div(image_width as f32);
+    let pixel_delta_v = viewport_v.clone().div(image_height as f32);
 
-            ppm.set_color_data(i, j, Color { x: r, y: g, z: 0.0 });
+    let viewport_upper_left = camera_center
+        .clone()
+        .add(Vec3::new(0.0, 0.0, focal_length).negate())
+        .add(viewport_u.clone().div(2.0).negate())
+        .add(viewport_v.clone().div(2.0).negate());
+
+    let pixel00_loc = viewport_upper_left
+        .clone()
+        .add(pixel_delta_u.clone().add(pixel_delta_v).mul(0.5));
+
+    for i in 0..image_height {
+        for j in 0..image_width {
+            let pixel_center = pixel00_loc
+                .clone()
+                .add(pixel_delta_u.clone().mul(i as f32))
+                .add(pixel_delta_v.clone().mul(j as f32));
+
+            let ray_direction = pixel_center.clone().add(camera_center.clone().negate());
+
+            let ray = Ray::new(camera_center.clone(), ray_direction.clone());
+            let ray_color = ray.get_color() as Color;
+
+            print!("\r[scanline remaining: {}]", (image_height - i));
+
+            ppm.set_color_data(i, j, ray_color);
         }
     }
     ppm.write_ppm();
