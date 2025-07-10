b38e1f78e106548d5a0d90a6d1ad0a6f711e3469
Shivang Upadhyay <shivangu@linux.ibm.com>
2025-07-10 12:12:04 +0530

    initial implementation for this program
    this is a rust project, that is developed for the solve purpose for me
    to learn this language, so far its simple and nice.  more nice than C.

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::ptr::read;
+
+#[derive(Clone, Copy)]
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn format(self) -> String {
+        return format!("{} {} {}\n", self.r, self.g, self.b);
+    }
+}
+struct Ppm {
+    height: usize,
+    width: usize,
+    file_name: &'static str,
+
+    maximum_color_value: usize,
+    file: Option<File>,
+
+    color_data: Vec<Vec<Color>>,
+}
+
+impl Ppm {
+    pub fn new(file_name: &'static str, height: usize, width: usize) -> Self {
+        Ppm {
+            file_name: file_name,
+            width: width,
+            height: height,
+            file: None,
+            maximum_color_value: 255,
+            color_data: vec![vec![Color { r: 0, g: 0, b: 0 }; width]; height],
+        }
+    }
+    pub fn create_file(&mut self) -> &mut Self {
+        self.file = Some(
+            OpenOptions::new()
+                .create(true)
+                .append(false)
+                .write(true)
+                .open(self.file_name)
+                .expect("meh"),
+        );
+        self
+    }
+    pub fn add_header(&mut self) -> &mut Self {
+        match &mut self.file {
+            Some(f) => {
+                let header = format!(
+                    "P3\n{} {}\n{}\n",
+                    self.height, self.width, self.maximum_color_value
+                );
+                f.write_all(header.as_bytes()).expect("file write failed");
+            }
+            None => {
+                panic!("file is none");
+            }
+        }
+        self
+    }
+
+    pub fn set_color_data(&mut self, x: usize, y: usize, c: Color) -> &mut Self {
+        assert!(x < self.height && y < self.width);
+        self.color_data[x][y] = c;
+        self
+    }
+    pub fn write_ppm(&mut self) {
+        let file = match &mut self.file {
+            Some(file) => file,
+            None => panic!("file not there"),
+        };
+
+        for i in 0..self.height {
+            for j in 0..self.width {
+                file.write_all(self.color_data[i][j].format().as_bytes())
+                    .expect("failed to write");
+            }
+        }
+    }
+}
+
+pub fn main() {
+    let mut ppm = Ppm::new("output.ppm", 256, 256);
+    let red_color: Color = Color { r: 255, g: 0, b: 0 };
+    ppm.create_file().add_header();
+
+    for i in 0..256 {
+        for j in 0..256 {
+            let mut r = (i as f32) / 255.0;
+            let mut g = (j as f32) / 255.0;
+            let b: u8 = 100;
+            r *= 255.0;
+            g *= 255.0;
+            let r1 = r as u8;
+            let g1 = g as u8;
+            // println!("{} {} {}\n", r1, g1 , b);
+            ppm.set_color_data(i, j, Color { r: r1, g: g1, b });
+        }
+    }
+
+    ppm.write_ppm();
+
+    drop(ppm);
+}
